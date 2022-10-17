b42e436a4c24d6dc2bcd72e75fd9fd1e12cf991d
Trần Hùng <87459538+TranHungg@users.noreply.github.com>
2022-10-17 14:08:12 +0700

    Update Report page
    Fix chart and product

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -6,6 +6,153 @@ import ReportProduct from "../components/reportProduct";
 import BtnMedium from "../images/Button-Medium.png";
 const Report = () => {
   const box_shadow = { boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" };
+
+  const checkouts = [
+    {
+      address: "Ho chi minh, Viet Nam",
+      checkoutId: 201,
+      customer: "abcd@gm.uit.edu.vn",
+      price: 20,
+      status: "Waiting",
+      checkoutTime: "2022/10/16",
+    },
+    {
+      address: "Ho chi minh, Viet Nam",
+      checkoutId: 203,
+      customer: "abcd@gm.uit.edu.vn",
+      price: 40,
+      status: "Waiting",
+      checkoutTime: "2022/10/02",
+    },
+    {
+      address: "Ho chi minh, Viet Nam",
+      checkoutId: 203,
+      customer: "abcd@gm.uit.edu.vn",
+      price: 10,
+      status: "Delivering",
+      checkoutTime: "2022/09/02",
+    },
+    {
+      address: "Ho chi minh, Viet Nam",
+      checkoutId: 203,
+      customer: "abcd@gm.uit.edu.vn",
+      price: 100,
+      status: "Delivering",
+      checkoutTime: "2022/08/02",
+    },
+    {
+      address: "Ho chi minh, Viet Nam",
+      checkoutId: 203,
+      customer: "abcd@gm.uit.edu.vn",
+      price: 30,
+      status: "Delivering",
+      checkoutTime: "2022/07/02",
+    },
+  ];
+
+  // lay date today
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+
+  const calSoldProductAmountData = () => {
+    // init
+    var result = [0, 0, 0, 0, 0, 0];
+
+    // gan null vao thang chua co data
+    const temp = 12 - today.getMonth() - 1;
+    for (let i = temp; i > 0; i--) {
+      result[6 - i] = null;
+    }
+
+    // lay data gan vao cac thang da co data
+    checkouts.forEach((element) => {
+      const date = new Date(element.checkoutTime);
+      switch (date.getMonth() + 1) {
+        case 7:
+          result[0]++;
+          break;
+        case 8:
+          result[1]++;
+          break;
+        case 9:
+          result[2]++;
+          break;
+        case 10:
+          result[3]++;
+          break;
+        case 11:
+          result[4]++;
+          break;
+        case 12:
+          result[5]++;
+          break;
+      }
+    });
+    console.log(result);
+    return result;
+  };
+
+  const calSoldEarningData = () => {
+    // init
+    var result = [0, 0, 0, 0, 0, 0];
+
+    // gan null vao thang chua co data
+    const temp = 12 - today.getMonth() - 1;
+    for (let i = temp; i > 0; i--) {
+      result[6 - i] = null;
+    }
+
+    // lay data gan vao cac thang da co data
+    checkouts.forEach((element) => {
+      const date = new Date(element.checkoutTime);
+      switch (date.getMonth() + 1) {
+        case 7:
+          result[0] += element.price;
+          break;
+        case 8:
+          result[1] += element.price;
+          break;
+        case 9:
+          result[2] += element.price;
+          break;
+        case 10:
+          result[3] += element.price;
+          break;
+        case 11:
+          result[4] += element.price;
+          break;
+        case 12:
+          result[5] += element.price;
+          break;
+      }
+    });
+    console.log(result);
+    return result;
+  };
+
+  const chartData = {
+    d1: {
+      title: "Sold Product Amount",
+      data: calSoldProductAmountData(),
+    },
+    d2: {
+      title: "Sold Earning",
+      data: calSoldEarningData(),
+    },
+  };
+
+  // TODO
+  const chartSmallData = {
+    d1: {
+      title: "Pending Orders Amount",
+      data: [33, 53, 85, 41, 44, 65],
+    },
+    d2: {
+      title: "Sold Orders Amount",
+      data: [33, 25, 35, 51, 54, 76],
+    },
+  };
+
   return (
     <div className="w-[100%] bg-[#F9F9F9]">
       <div className="w-[90%] ml-[5%] mt-[0%]">
@@ -141,7 +288,7 @@ const Report = () => {
               </div>
             </div>
           </div>
-          <Chart />
+          <Chart d={chartData} />
           <div className="ml-[8%]">
             {/* table  */}
             <ReportProduct></ReportProduct>
