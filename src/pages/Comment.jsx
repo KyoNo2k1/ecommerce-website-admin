0aa4d4a45d2359e4df1065867db7891cf1d330b9
Trần Hùng <87459538+TranHungg@users.noreply.github.com>
2022-10-21 16:25:03 +0700

    #21 add UI of comment page
    - complete admin's comment UI

--- src/pages/Comment.jsx
+++ src/pages/Comment.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import productImage from "../images/productImage.png";
 
 const Comment = () => {
   const comments = [
     {
       id: 1,
-      productID: "Nguyen Van A",
+      productName: "Chair",
       customer: "abcd@gm.uit.edu.vn",
       content: "Very ugly, loremmmmmmmmmmmmmmmmmmmm",
       postDate: "02-02-2022",
     },
     {
       id: 2,
-      productID: "Nguyen Van A",
+      productName: "Chair",
       customer: "abcd@gm.uit.edu.vn",
       content: "Very ugly, loremmmmmmmmmmmmmmmmmmmm",
       postDate: "02-02-2022",
@@ -23,30 +24,73 @@ const Comment = () => {
   return (
     <div className="px-24 my-2">
       <table className="w-[100%]">
-        <tbody>
+        <thead>
           <tr>
             <th>STT</th>
-            <th>Product ID</th>
+            <th>Product name</th>
             <th>Customer</th>
             <th>Content</th>
             <th>Post Date</th>
             <th></th>
           </tr>
-          {comments?.map((comment) => (
-            <tr className="border-b-2" key={comment.id}>
+        </thead>
+        {comments?.map((comment) => (
+          <tbody className="border-b-2" key={comment.id}>
+            <tr>
               <td>{comment.id}</td>
-              <td>{comment.productID}</td>
-              <td>{comment.customer}</td>
-              <td className="text-ellipsis overflow-hidden ...">
+              <td>{comment.productName}</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td rowSpan={3} colSpan={2}>
+                <img src={productImage} alt="" className="mt-[-16px]"></img>
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                {comment.customer}
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
                 {comment.content}
               </td>
-              <td>{comment.postDate}</td>
-              <td className="border-none">
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                {comment.postDate}
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
                 <FontAwesomeIcon icon={faTrashCan} />
               </td>
             </tr>
-          ))}
-        </tbody>
+            <tr>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                {comment.customer}
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                {comment.content}
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                {comment.postDate}
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                <FontAwesomeIcon icon={faTrashCan} />
+              </td>
+            </tr>
+            <tr>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                {comment.customer}
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                {comment.content}
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                {comment.postDate}
+              </td>
+              <td className="border-y-[1px] border-y-[rgb(82 82 82)]">
+                <FontAwesomeIcon icon={faTrashCan} />
+              </td>
+            </tr>
+          </tbody>
+        ))}
       </table>
     </div>
   );
